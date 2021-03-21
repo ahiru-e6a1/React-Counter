3c7e728783ba6c49551b1d881116472022a1484a
ahiru-e6a1 <a.h.i.r.u@icloud.com>
2021-03-21 23:01:37 +0900

    Counter 作成

---
+++ counter/src/components/MinusButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Button from "./Button";
+
+const MinusButton = ({ text, onClickPlusButton }) => {
+  return (
+    <div>
+      <Button text={text} onClickButton={onClickPlusButton} />
+    </div>
+  );
+};
+
+export default MinusButton;
---
+++ counter/src/components/PlusButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Button from "./Button";
+
+const plusButton = ({ text, onClickPlusButton }) => {
+  return (
+    <div>
+      <Button text={text} onClickButton={onClickPlusButton} />
+    </div>
+  );
+};
+
+export default plusButton;
